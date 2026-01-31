4c7889830cab6d197c2b5bdc0b2628dd403fda42
moriaki-maetsu <159523732+moriaki-maetsu@users.noreply.github.com>
2026-01-31 21:19:37 +0900

    TitleとResoultの編集

--- GGJ2026_TeamF/GGJ2026_TeamF/Source/Scene/Result/ResultScene.cpp
+++ GGJ2026_TeamF/GGJ2026_TeamF/Source/Scene/Result/ResultScene.cpp
@@ -1,16 +1,38 @@
 ﻿#include "ResultScene.h"
+#include"DxLib.h"
+#include"../../Utility/AssetContainer.h"
+#include"../../Utility/InputManager.h"
+
+int hero_red, hero_blue, hero_green, hero_yellow,hero_pink;
+
 
 void ResultScene::Initialize()
 {
+	AssetContainer* container = AssetContainer::Get();
+	 hero_red = container->GetImages("character_red_01.png")[0];
+	 hero_blue = container->GetImages("character_blue_01.png")[0];
+	 hero_green = container->GetImages("character_green_01.png")[0];
+	 hero_yellow = container->GetImages("character_yellow_01.png")[0];
+	 hero_pink = container->GetImages("character_pink_01.png")[0];
+
 }
 
 eSceneType ResultScene::Update(float delta_second)
 {
+
 	return GetNowSceneType();
 }
 
 void ResultScene::Draw() const
 {
+
+	DrawRotaGraph(1280 / 2, 720 / 2, 0.5,0.0, hero_red, TRUE);
+	DrawRotaGraph(1280 / 2+100, 720 / 2, 0.5,0.0, hero_blue, TRUE);
+	DrawRotaGraph(1280 / 2-100, 720 / 2, 0.5,0.0, hero_green, TRUE);
+	DrawRotaGraph(1280 / 2+250, 720 / 2, 0.5,0.0, hero_yellow, TRUE);
+	DrawRotaGraph(1280 / 2-250, 720 / 2, 0.5,0.0, hero_pink, TRUE);
+
+
 }
 
 void ResultScene::Finalize()
--- GGJ2026_TeamF/GGJ2026_TeamF/Source/Scene/Title/TitleScene.cpp
+++ GGJ2026_TeamF/GGJ2026_TeamF/Source/Scene/Title/TitleScene.cpp
@@ -2,23 +2,91 @@
 #include "TitleScene.h"
 #include "../../Utility/InputManager.h"
 
+#define	D_WINDOW_SIZE_X	(1280)	// Windowサイズ（幅）
+#define	D_WINDOW_SIZE_Y	(720)	// Windowサイズ（高さ）
+
+#define START_BUTTON_UPPER_X 1280/2-100
+#define START_BUTTON_LOWER_X 1280/2+100
+#define START_BUTTON_UPPER_Y 720/2-20
+#define START_BUTTON_LOWER_Y 720/2+20
+
+#define END_BUTTON_UPPER_X 1280/2-100
+#define END_BUTTON_LOWER_X 1280/2+100
+#define END_BUTTON_UPPER_Y 720/2+40
+#define END_BUTTON_LOWER_Y 720/2+80
+
+int white = GetColor(255, 255, 255);
+int black = GetColor(0, 0, 0);
+int red = GetColor(255, 0, 0);
+int startboxcolor, endboxcolor;
+
 void TitleScene::Initialize()
 {
+	
 }
 
 eSceneType TitleScene::Update(float delta_second)
 {
 	InputManager* input =InputManager::Get();
-	if (input->GetMouseState(MOUSE_INPUT_LEFT) == eInputState::eClick)
+
+	if (input->GetMouseLocation().x >= START_BUTTON_UPPER_X && input->GetMouseLocation().x <= START_BUTTON_LOWER_X)
 	{
-		return eSceneType::ePhaseOne;
+		if (input->GetMouseLocation().y >= START_BUTTON_UPPER_Y && input->GetMouseLocation().y <= START_BUTTON_LOWER_Y)
+		{
+			startboxcolor = red;
+
+			if (input->GetMouseState(MOUSE_INPUT_LEFT) == eInputState::eClick)
+			{
+				
+				return eSceneType::ePhaseOne;
+			}
+		}
+		else
+		{
+			startboxcolor = white;
+		}
 	}
+	else
+	{
+		startboxcolor = white;
+	}
+
+	if (input->GetMouseLocation().x >= END_BUTTON_UPPER_X && input->GetMouseLocation().x <= END_BUTTON_LOWER_X)
+	{
+		if (input->GetMouseLocation().y >= END_BUTTON_UPPER_Y && input->GetMouseLocation().y <= END_BUTTON_LOWER_Y)
+		{
+			endboxcolor = red;
+
+			if (input->GetMouseState(MOUSE_INPUT_LEFT) == eInputState::eClick)
+			{
+				return eSceneType::eResult;
+			}
+		}
+		else
+		{
+			endboxcolor = white;
+		}
+	}
+	else
+	{
+		endboxcolor = white;
+	}
+	
+
+
+
 	return GetNowSceneType();
 }
 
 void TitleScene::Draw() const
 {
-	DrawString(0, 0, "タイトル画面", GetColor(255, 255, 255));
+
+	DrawBox(START_BUTTON_UPPER_X, START_BUTTON_UPPER_Y, START_BUTTON_LOWER_X, START_BUTTON_LOWER_Y, startboxcolor, TRUE);
+	DrawBox(END_BUTTON_UPPER_X, END_BUTTON_UPPER_Y, END_BUTTON_LOWER_X, END_BUTTON_LOWER_Y, endboxcolor, TRUE);
+	DrawString(0, 0, "タイトル画面", white);
+	DrawString(START_BUTTON_UPPER_X, START_BUTTON_UPPER_Y, "スタート", black);
+	DrawString(END_BUTTON_UPPER_X, END_BUTTON_UPPER_Y, "エンド", black);
+	
 }
 
 void TitleScene::Finalize()
