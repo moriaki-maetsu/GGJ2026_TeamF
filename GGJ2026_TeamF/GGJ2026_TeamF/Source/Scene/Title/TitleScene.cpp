178115fc816fc72025d29ddee574a8d9dc03545a
nashiro1104 <kyoichirou-nashiro-k24h@stu.kbc.ac.jp>
2026-01-31 17:07:55 +0900

    Update TitleScene.cpp

--- GGJ2026_TeamF/GGJ2026_TeamF/Source/Scene/Title/TitleScene.cpp
+++ GGJ2026_TeamF/GGJ2026_TeamF/Source/Scene/Title/TitleScene.cpp
@@ -1,4 +1,6 @@
-﻿#include "TitleScene.h"
+﻿#include "DxLib.h"
+#include "TitleScene.h"
+#include "../../Utility/InputManager.h"
 
 void TitleScene::Initialize()
 {
@@ -6,11 +8,17 @@ void TitleScene::Initialize()
 
 eSceneType TitleScene::Update(float delta_second)
 {
+	InputManager* input =InputManager::Get();
+	if (input->GetMouseState(MOUSE_INPUT_LEFT) == eInputState::eClick)
+	{
+		return eSceneType::ePhaseOne;
+	}
 	return GetNowSceneType();
 }
 
 void TitleScene::Draw() const
 {
+	DrawString(0, 0, "タイトル画面", GetColor(255, 255, 255));
 }
 
 void TitleScene::Finalize()
